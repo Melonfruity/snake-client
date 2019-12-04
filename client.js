ffb532fb0e6cc1235d26f6821d122d938fd059b6
melonfruity <eric.luo24@gmail.com>
2019-12-04 12:42:30 -0500

    added the sent name function after connection is established

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+  });
+
+  conn.write(`Name: EL4`, () => {
+    console.log('Sent name');
+  });
+  return conn;
+}
+
+module.exports = {connect}
